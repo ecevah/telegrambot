e03bfabe4ddabc5b2f3e3ed29b4e9f0cf837a1c2
ahmet <eecevah@gmail.com>
2022-08-11 21:17:23 +0300

    api key değiştirildi

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const app = express();
  app.listen(5858);
 
  const init= async() => {
-   console.log(config.simNet.apiKey);
    const response = await axios.get(`${config.simNet.url}/v1/user/profile`,{
       headers: {
 
             "Authorization": `Bearer ${config.simNet.apiKey}`, 
             "Content-Type": "application/json"
       }});
-   console.log(config.simNet.apiKey);
+   console.log(response.data);
    
 
 
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     simNet: {
-        apiKey: 'a201a244c64c4fcfbfe2c9aa8da628cc',
+        apiKey: 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTAyMjQyNzgsImlhdCI6MTY1ODY4ODI3OCwicmF5IjoiNzAxM2E0MTczMGE4ZTI4NWVlZWMxOWE0MDQwMzIyOGYiLCJzdWIiOjExMjY0ODB9.vhj8Ig68dbao4hTV-XQAyh84Ulx10SgKsC-w1F7qbswwbzBiKaKRf6dHfo36XOSAFuwFYyCSt4SN1v6FN5OhaJlOHXPgdZ5xVqrAPG51XoScgMr8rs8AX4wuQnCXKq-OSkVMeOyggZGCydrKTV9d64ku9E9iYm-KB8knK1uKIie8ub6Ydn2WD14b-V3HOSUSu209F5uayDk_Z18VYAnLfRvY_L0v_1n0k97pqO8enFMVTabomOuBlSX-KA4qlVWZXF2N9vVmi50k16l0LLBNBEfDxyi8wICKO8ETv1GuwLv2EMHBnLrhB3ylegPjC9-NGDgASzpFNJfjjetG5aqISw',
         delay: 180,
         rubLimit: 28,
         country: ["turkey", "mongolia", "vietnam"],
